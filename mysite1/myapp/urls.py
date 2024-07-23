763cb5daac46165edf3945be0abfe51330d4818d
A737580 <lalai33@mail.ru>
2024-07-23 21:19:32 +0300

    created and styled addItem form

--- mysite1/myapp/urls.py
+++ mysite1/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from myapp.views import index, indexItem
+from myapp.views import index, indexItem, add_item
 
 app_name = 'myapp'
 
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('<int:my_id>/', indexItem, name='detail'),
     #http://127.0.0.1:8000/myapp/hello
-
+    path("additem/", add_item, name="add_item"),
 
     
 
