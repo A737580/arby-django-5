c869795f99bb34474af8c99c9ed3d2f0122c1995
A737580 <lalai33@mail.ru>
2024-07-25 23:08:57 +0300

    added update delete methods Item

--- mysite1/myapp/views.py
+++ mysite1/myapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
@@ -18,16 +18,38 @@ def indexItem(request, my_id):
     }
     return render(request, "myapp/detail.html", context=context)
 
-@login_required
 def add_item(request):
     if request.method == "POST":
         name = request.POST.get("name")
         price = request.POST.get("price")
         description = request.POST.get("description")
         image = request.FILES["upload"]
-        seller = request.user
         item = Product(
-            name=name, price=price, description=description, image=image, seller=seller
+            name=name, price=price, description=description, image=image
         )
         item.save()
-    return render(request, "myapp/additem.html")
+    return render(request, "myapp/updateItem.html")
+
+def update_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    context = {
+        'item': item
+    }
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get("upload", item.image)
+        item.save()
+        return redirect('/myapp/')
+    return render(request, "myapp/updateItem.html",context)
+
+def delete_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    context = {
+        'item': item
+    }
+    if request.method == "POST":
+        item.delete()
+        return redirect('/myapp/')
+    return render(request, "myapp/deleteItem.html",context)
