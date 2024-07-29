c6e1e33e25b1bc7d560951738126acb8cfd11159
A737580 <lalai33@mail.ru>
2024-07-29 23:10:46 +0300

    admin settings

--- mysite1/myapp/admin.py
+++ mysite1/myapp/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 
 from .models import Product
 
-admin.site.register(Product)
+
+admin.site.site_header = "My Django app"
+admin.site.site_title = "Title of Django app"
+admin.site.index_title = "My admin site"
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'image','description')
+    search_fields = ('name',)  #Это обязательно картежи
+    list_editable = ('price', 'description')
+    actions = ('make_zero',)
+
+    def make_zero(self,request,queryset):
+        queryset.update(price=0)
+
+
+admin.site.register(Product, ProductAdmin)
+
